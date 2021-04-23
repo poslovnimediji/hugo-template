799775e6ddb3fbd67aefb059d29421e6170ade78
martinjagodic <jagodicmartin1@gmail.com>
2021-04-23 08:56:55 +0200

    add css autoprefixer

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ const purgecss = require('@fullhuman/postcss-purgecss')
 
 module.exports = {
   plugins: [
+    require('autoprefixer'),
     purgecss({
       content: ['./layouts/**/*.html', './assets/scripts/*.js'],
       safelist: [
