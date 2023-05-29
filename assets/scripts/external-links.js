fe793b0712633227b9e6f47e2b02e2dca92d097c
Martin Jagodic <jagodicmartin1@gmail.com>
2023-05-29 16:02:26 +0200

    change target blank to icon on external links
    https://github.com/poslovnimediji/pm-procesi/issues/22

--- assets/scripts/external-links.js
+++ assets/scripts/external-links.js
@@ -2,7 +2,7 @@ function externalLinks () {
   Array.from(document.links).forEach((el) => {
     const link = el
     if (link.hostname && link.hostname !== window.location.hostname) {
-      link.target = '_blank'
+      link.classList.add('external')
       link.res = 'noopener'
     }
   })
