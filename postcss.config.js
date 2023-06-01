2dc369800088fe8df49c2edd724066bf3f896890
Martin Jagodic <jagodicmartin1@gmail.com>
2023-06-01 14:15:36 +0200

    update dependencies and linters

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,16 @@ module.exports = {
   plugins: [
     require('autoprefixer'),
     purgecss({
-      content: ['./layouts/**/*.html', './assets/scripts/*.js'],
+      content: [
+        './layouts/*.html',
+        './layouts/**/*.html',
+        './assets/scripts/*.js',
+        './assets/scripts/**/*.js',
+        './themes/**/layouts/*.html',
+        './themes/**/layouts/**/*.html',
+        './themes/**/assets/scripts/*.js',
+        './themes/**/assets/scripts/**/*.js',
+      ],
       safelist: [
         // add classes found only in js
       ],
