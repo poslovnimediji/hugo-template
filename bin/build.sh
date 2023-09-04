74bdc3381c6fe5ef7c7a59292f4dcdae1d50935c
Martin Jagodic <jagodicmartin1@gmail.com>
2023-09-04 14:34:08 +0200

    change CMS branch to be current deploy branch

--- bin/build.sh
+++ bin/build.sh
@@ -3,10 +3,12 @@
 if [[ "$CONTEXT" = "production" ]]
 then
   echo ----- PRODUCTION
+  sed -i "s|__BRANCH__|${HEAD}|g" static/admin/config.yml
   hugo --gc --minify -b $URL
   status=$?
 else
   echo +++++ PREVIEW
+  sed -i "s|__BRANCH__|${HEAD}|g" static/admin/config.yml
   hugo --gc -b $DEPLOY_PRIME_URL
   status=$?
 fi
